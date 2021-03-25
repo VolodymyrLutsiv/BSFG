22d80f125f39df4c8d80440d8f0e0f37028295ac
VolodymyrLutsiv <Vova_96lu@ukr.net>
2021-03-25 18:51:44 +0200

    2

--- ru.bsfg/src/main/java/ru/bsfg/data/User.java
+++ ru.bsfg/src/main/java/ru/bsfg/data/User.java
@@ -2,7 +2,8 @@ package ru.bsfg.data;
 
 public enum User {
     BRAT0K("BRAT0K","Lv197358246"),
-    DEV1L("westua","iphonese4018");
+    DEV1L("westua","iphonese4018"),
+    D1REWOLF("d1rewolf","iphonese4018");
 
     private String username;
     private String password;
